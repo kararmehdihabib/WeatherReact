039848dea099902a542ad17e39ab15baece9b93d
kararmehdi <karar.habib2@metropolia.fi>
2016-10-17 22:21:38 +0300

    Scss support

--- app/app.jsx
+++ app/app.jsx
@@ -7,7 +7,7 @@ var About =require('About');
 var Examples =require('Examples');
 
 //App CSS
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
 // Load Foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
